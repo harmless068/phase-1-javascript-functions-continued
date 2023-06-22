0dc44f82f316e1d4228ddeb62cbda506ea87e842
osman <harmless068@gmail.com>
2023-06-22 09:22:04 +0300

    javasript-continued function

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// code your solution here
+// Implementing saturdayFun function
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+  // Implementing mondayWork function expression
+  const mondayWork = function (activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+  };
+  
+  // Implementing wrapAdjective function
+  function wrapAdjective(flair = "*") {
+    return function (adjective) {
+      return `You are ${flair}${adjective}${flair}!`;
+    };
+  }
+  
+  module.exports = {
+    saturdayFun,
+    mondayWork,
+    wrapAdjective,
+  };
+  
